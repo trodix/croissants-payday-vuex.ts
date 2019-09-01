8fcfa3f02e511776aaaba8b18545def23a093244
Sebastien Vallet <sebastien.vallet89@gmail.com>
2019-09-01 19:40:36 +0200

    vuex getting paydays works, fuck yeah

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="container">
-    <ul v-for="payday in paydays" :key="payday.player">
-      <li>{{ paydays.player }}</li>
-    </ul>
+    <v-flex>
+      <template v-if="paydays !== undefined && paydays.length > 0">
+        <ul v-for="payday in paydays" :key="payday._id">
+          <li>{{ payday.player }}</li>
+        </ul>
+      </template>
+      <template v-else>
+        <v-progress-circular indeterminate></v-progress-circular>
+        <span>Loading data</span>
+      </template>
+    </v-flex>
+
   </div>
 </template>
 
 <script lang="ts">
 
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component, PropSync } from 'vue-property-decorator';
 import PaydaysModule from '../store/modules/Paydays';
 import { Payday } from '../store/models';
 
@@ -16,13 +25,17 @@ import { Payday } from '../store/models';
 @Component({})
 export default class Dashboard extends Vue {
 
-  paydays: Payday[] = [];
-
   constructor() {
     super();
     PaydaysModule.getAllPaydays();
-    this.paydays = PaydaysModule.paydays;
-    console.log(this.paydays);
+  }
+
+  get paydays(): Payday[] {
+    return PaydaysModule.paydays;
+  }
+
+  getPaydays() {
+    PaydaysModule.getAllPaydays();
   }
 
 }
